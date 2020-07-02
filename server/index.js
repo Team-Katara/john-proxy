db57c55f208e97758a55ac920f95427e026b517e
xApnea <jcbruno150@gmail.com>
2020-07-02 19:35:34 -0400

    Server routes requests and serves static files appropriately

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const port = process.env.PORT;
+const cors = require('cors');
+const morgan = require('morgan');
+const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.static('dist'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Proxy server listening at http://localhost:${port}`))
